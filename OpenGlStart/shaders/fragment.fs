8fde6c8cd9f571f03014110aebf7ac7bc333f95a
emiel <emielvdbrink@gmail.com>
2018-07-29 21:46:56 +0200

    added texture rendering :D

--- OpenGlStart/shaders/fragment.fs
+++ OpenGlStart/shaders/fragment.fs
@@ -2,15 +2,11 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
-in vec3 vertexPos;
+in vec2 texCoord;
+
+uniform sampler2D texture1;
 
 void main()
 {
-	vec3 pos = vertexPos;
-	if (pos.x < 0)
-	{
-		pos.x = -pos.x;
-	}
-
-    FragColor = vec4(pos, 1.0f);
+    FragColor = texture(texture1, texCoord);
 }
