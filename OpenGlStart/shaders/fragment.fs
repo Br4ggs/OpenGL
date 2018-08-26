7c14e0971a510f50135cb5ce7397a9d99cb41e48
emiel <emielvdbrink@gmail.com>
2018-08-26 19:00:22 +0200

    added fps-style navigation

--- OpenGlStart/shaders/fragment.fs
+++ OpenGlStart/shaders/fragment.fs
@@ -5,9 +5,8 @@ in vec2 texCoord;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
-uniform float mixVal;
 
 void main()
 {
-    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), mixVal);
+    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.5f);
 }
