bbda7b56f3df66edd2cc8c14105bc96cef10fbaf
emiel <emielvdbrink@gmail.com>
2018-08-28 22:16:17 +0200

    reworked camera.h file

--- OpenGlStart/Camera.h
+++ OpenGlStart/Camera.h
@@ -1,1 +1,56 @@
-#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+enum Camera_Movement
+{
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+// default values
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM = 45.0f;
+
+class Camera
+{
+public:
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 up;
+	glm::vec3 right;
+	glm::vec3 worldUp;
+
+	float yaw;
+	float pitch;
+
+	float movementSpeed;
+	float mouseSensitivity;
+	float zoom;
+
+	//REQUIRES INITIALISATION LIST
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
+
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
+
+	glm::mat4 getViewMatrix();
+
+	void processKeyBoard(Camera_Movement direction, float deltaTime);
+
+	void processMouseMovement(float xOffset, float yOffset, GLboolean constrainPitch = true);
+
+	void processMouseScroll(float yOffset);
+
+private:
+	void updateCameraVectors();
+};
+
+#endif
