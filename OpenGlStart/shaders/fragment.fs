95698a2649f01f1c004fe72a895da9c57ec5b1c7
Emiel van den Brink <emielvdbrink@gmail.com>
2019-09-09 16:34:01 +0200

    added wave effect to texture on cube

--- OpenGlStart/shaders/fragment.fs
+++ OpenGlStart/shaders/fragment.fs
@@ -6,7 +6,26 @@ in vec2 texCoord;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
+uniform float time;
+
+const float A = 0.03;
+const float B = 200.0;
+const float C = 5.0;
+
+const float D = 0.003;
+const float E = 13.0;
+const float F = 9.0;
+float x;
+float y;
+
 void main()
 {
-    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.5f);
+    //FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.5f);
+	gl_FragColor =  texture2D(texture1, texCoord);
+	gl_FragColor.a = 1.0; //Best to make sure nothing seems transparent
+	float x =  A * sin(B * texCoord.x) * sin(C * time);
+	float y =  D * sin(E * texCoord.y) * sin(F * time);
+	vec2 c = vec2(texCoord.x + x, texCoord.y +y);
+	vec4 diffuse_color =  texture2D(texture1, c);
+	gl_FragColor = diffuse_color;
 }
